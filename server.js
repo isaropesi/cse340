b18fe9f73402851177395eba321191a1095fbd51
isaropesi <sil22028@byui.edu>
2025-11-21 20:33:32 -0300

    Sessions and Messages

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const session = require("express-session")
+const pool = require('./database/')
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 // Inventory route
 const inventoryRoute = require("./routes/inventoryRoute")
 // Utility file required for error handling and other functions
@@ -22,6 +30,19 @@ const utilities = require("./utilities/")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
 
 /* ***********************
  * Routes
